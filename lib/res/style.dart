f2b3fae510ddf11a8dd11d317b5bc23c45dc0f6e
liuwei <liuwei@suishenchailv.com>
2019-07-28 16:49:43 +0800

    修复类名冲突

--- lib/res/style.dart
+++ lib/res/style.dart
@@ -6,15 +6,15 @@ import '../res/dimens.dart';
 class TextStyles {
   static TextStyle listTitle = TextStyle(
     fontSize: Dimens.font_sp16,
-    color: Colors.text_dark,
+    color: LocalColors.text_dark,
     fontWeight: FontWeight.bold,
   );
   static TextStyle listContent = TextStyle(
     fontSize: Dimens.font_sp14,
-    color: Colors.text_normal,
+    color: LocalColors.text_normal,
   );
   static TextStyle listExtra = TextStyle(
     fontSize: Dimens.font_sp12,
-    color: Colors.text_gray,
+    color: LocalColors.text_gray,
   );
 }
